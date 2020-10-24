0dbae760c44ecac0f9545c377cdd61d5ff5a8384
gregoryvanko <gregory.vankoninckxloo@gmail.com>
2020-10-24 14:31:58 +0200

    auto save

--- Worker.js
+++ Worker.js
@@ -44,7 +44,7 @@ class Worker {
         this._MyApp.LogAppliInfo("Call ApiWorker Data:" + JSON.stringify(Data), User, UserId)
         if (Data.Fct == "ButtonPressed"){
             Res.json({Error: false, ErrorMsg: "Worker", Data: "Worker Started by: " + Data.Name})
-            this.ButtonPressed(Data.Name)
+            this.ButtonPressed(Data.Name, User, UserId)
         } else if(Data.Fct == "Ping"){
             Res.json({Error: false, ErrorMsg: "Worker", Data: "Pong"})
         } else if(Data.Fct == "GetConfig"){
@@ -76,8 +76,81 @@ class Worker {
         })
     }
 
-    ButtonPressed(ButtonName){
-        //  ToDo Start Button Fct
+    /**
+     * Get de la config sous forme de promise
+     * @param {String} User nom du user
+     * @param {Int} UserId user id
+     */
+    PromiseGetConfig(Key, User, UserId){
+        return new Promise(resolve => {
+            let ReponseConfig = new Object()
+            ReponseConfig.Error = true
+            ReponseConfig.ErrorMsg = ""
+            ReponseConfig.Data = null
+    
+            let me = this
+            const Querry = {[this._MongoConfigCollection.Key]: Key}
+            const Projection = { projection:{_id: 0, [this._MongoConfigCollection.Value]: 1}}
+            this._Mongo.FindPromise(Querry, Projection, this._MongoConfigCollection.Collection).then((reponse)=>{
+                if(reponse.length == 0){
+                    ReponseConfig.Error = false
+                    ReponseConfig.ErrorMsg = null
+                    ReponseConfig.Data = null
+                } else {
+                    ReponseConfig.Error = false
+                    ReponseConfig.ErrorMsg = null
+                    ReponseConfig.Data = reponse[0][this._MongoConfigCollection.Value]
+                }
+                resolve(ReponseConfig)
+            },(erreur)=>{
+                ReponseConfig.Error = true
+                ReponseConfig.ErrorMsg = "ApiWork GetConfig DB Error"
+                ReponseConfig.Data = null
+                me._MyApp.LogAppliError("ApiWork GetConfig DB error : " + erreur, User, UserId)
+                resolve(ReponseConfig)
+            })
+        })
+    }
+
+    async ButtonPressed(ButtonName, User, UserId){
+        // Si le woker ne confionne pas
+        if (!this._Status.IsRunning){
+            // Get de la config des GPIO
+            let ReponseGetGpioConfig = await this.PromiseGetConfig(this._MongoConfigCollection.GpioConfigKey, User, UserId)
+            // Si il existe une config
+            if(!ReponseGetGpioConfig.Error){
+                // Sil la config n'est pas null
+                if(ReponseGetGpioConfig.Data != null){
+                    // Chercher le bouton dans la config
+                    var FoundButton = ReponseGetGpioConfig.Data.find((element) => { return element.name == ButtonName })
+                    // Si le bouton est trouvé
+                    if(FoundButton){
+                        // Nom du program a trouver
+                        let ProgramForThisButton = FoundButton.custom.programname
+                        // Get de la config des programs
+                        let ReponseGetProgramsConfig = await this.PromiseGetConfig(this._MongoConfigCollection.ProgramConfigKey, User, UserId)
+                        // Si il existe une config de program
+                        if(!ReponseGetProgramsConfig.Error){
+                            // Sil la config n'est pas null
+                            if(ReponseGetProgramsConfig.Data != null){
+                                // Chercher le bouton dans la config
+                                var FoundProgram = ReponseGetProgramsConfig.Data.find((element) => { return element.Name == ProgramForThisButton })
+                                if (FoundProgram){
+                                    // Liste des steps du program
+                                    let ListOfStep = FoundProgram.ListOfSteps
+                                    // si le nombre de step est >0
+                                    if(ListOfStep.length > 0){
+                                        // Start du worker
+                                        // ToDo
+                                        //GlobalSendSocketIo("PlayProgram", "PlayWorker", this._ListOfProgram[Index].ListOfSteps)
+                                    } else {this._MyApp.LogAppliError("ButtonPressed: no list of step defined!", User, UserId)}
+                                } else {this._MyApp.LogAppliError("ButtonPressed: ProgramForThisButton not found in Program Config!", User, UserId)}
+                            }else {this._MyApp.LogAppliError("ButtonPressed: No Program config defined!", User, UserId)}
+                        }else {this._MyApp.LogAppliError("ButtonPressed: Error PromiseGetConfig for Program!", User, UserId)}
+                    } else {this._MyApp.LogAppliError("ButtonPressed: Button name not found in GPIO Config!", User, UserId)}
+                } else {this._MyApp.LogAppliError("ButtonPressed: No GPIO config defined!", User, UserId)}
+            } else {this._MyApp.LogAppliError("ButtonPressed: Error PromiseGetConfig for Gpio!", User, UserId)}
+        } else {this._MyApp.LogAppliError("Button pressed but worker is running", User, UserId)}
     }
 
     StartWorking(WorkerConfigList, User, UserId){
--- exemple.js
+++ exemple.js
@@ -2,8 +2,8 @@ let Aquagreen = require('./index').Aquagreen
 const Name = "AquagreenDev"
 const Port = 5000
 const Debug = true
+const UseRpiGpio = true
 //const RpiGpioAdress = "http://192.168.30.10:3000/api"     //RPI de prod
 const RpiGpioAdress = "http://192.168.10.21:3000/api"       //RPI de dev
-const UseRpiGpio = false
 let MyApp = new Aquagreen(Name, Port, Debug, RpiGpioAdress, UseRpiGpio)
 MyApp.Start()
